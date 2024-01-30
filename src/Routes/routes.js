9a577259c0f673b9095e1330c1ff536e8f849b64
nicolasestanislau <nicolas_estanislau@hotmail.com>
2024-01-30 15:22:51 -0300

    feat: finish crud

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -28,15 +28,47 @@ allRoutes.get("/todos", async (req, res) => {
 allRoutes.put("/todos", async (req, res) => {
   const { name, id, stauts } = req.body;
 
+  if (!id) {
+    return res.status(400).json("Id is mandatory");
+  }
+
+  const todoAlreadyExist = await prisma.todo.findUnique({ where: { id } });
+
+  if (!todoAlreadyExist) {
+    return res.status(404).json("Todo not exist");
+  }
   const todo = await prisma.todo.update({
     where: {
       id,
     },
     data: {
       name,
-      status,
     },
   });
+
+  return res.status(200).json(todo);
+});
+
+//Delete
+allRoutes.delete("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const intId = parseInt(id);
+
+  if (!intId) {
+    return res.status(400).json("Id is mandatory");
+  }
+
+  const todoAlreadyExist = await prisma.todo.findUnique({
+    where: { id: intId },
+  });
+
+  if (!todoAlreadyExist) {
+    return res.status(404).json("Todo not exist");
+  }
+
+  await prisma.todo.delete({ where: { id: intId } });
+  return res.status(200).send();
 });
 
 module.exports = allRoutes;
